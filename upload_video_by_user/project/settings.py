3c3154eed7d5187a9833d0957150ca892f57fa13
minji lee <48074724+mjlee2121@users.noreply.github.com>
2020-12-01 18:50:49 -0500

    Update settings.py

--- upload_video_by_user/project/settings.py
+++ upload_video_by_user/project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-g%c3)sr%gg&li3!_b#&&vy7i+2ig5a*9-b75$-nf0rt2)!q%%'
+SECRET_KEY = 'YOUR KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +76,8 @@ if os.getenv('GAE_APPLICATION', None):
         'default': {
             'ENGINE' : 'django.db.backends.mysql',
             'HOST' : '/cloudsql/django-deployment-297319:us-central1:polls-instance',
-            'USER' : 'minji',
-            'PASSWORD' : 'phantom21~',
+            'USER' : 'YOUR USER',
+            'PASSWORD' : 'YOUR PASSWORD',
             'NAME': 'main',
         }
         
